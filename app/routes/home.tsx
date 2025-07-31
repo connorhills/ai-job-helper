58051a297f6b57f8b5981340085035da470f634b
connorhills <connorhills2023@gmail.com>
2025-07-31 18:37:49 -0400

    Added basic home page + routing + assets

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Jobi" },
+    { name: "description", content: "Smart AI resume analyzer to help you achieve your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1 className="text-gradient">Track Your Application & Resume Ratings</h1>
+        <h2>Review your resumes receiving reliable AI-powered feedback!</h2>
+      </div>
+
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+  </main>
 }
